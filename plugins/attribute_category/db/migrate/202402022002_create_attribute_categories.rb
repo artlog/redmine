90368007eeaa2a6ae402af7676272cec06705c63
philippe L <philha@worteks.com>
2024-02-02 20:03:52 +0100

    Migrate from 4.2 ( was 7.1 )

--- plugins/attribute_category/db/migrate/202402022002_create_attribute_categories.rb
+++ plugins/attribute_category/db/migrate/202402022002_create_attribute_categories.rb
@@ -1,4 +1,4 @@
-class CreateAttributeCategories < ActiveRecord::Migration[7.1]
+class CreateAttributeCategories < ActiveRecord::Migration[4.2]
   def change
     create_table :attribute_categories do |t|
       t.string :name
