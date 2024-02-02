829115783165426d35ea0f0606e2381f98a1f6eb
philippe L <philha@worteks.com>
2024-02-02 19:15:17 +0100

    First working plugin
    - Fix IssueCustomField that can't be deleted : patch ActiveRecord
    - Fix css : use stylesheet_link_tag dedicated to plugin

--- plugins/attribute_category/init.rb
+++ plugins/attribute_category/init.rb
@@ -1,3 +1,35 @@
+
+
+# patch required ( current ActiveRecords 7.1.2 association bug ) for IssueCustmField.has_many
+module ActiveRecord
+  module Associations
+    # Returns the association instance for the given name, instantiating it if it doesn't already exist
+    def association(name) # :nodoc:
+      association = association_instance_get(name)
+
+      if association.nil?
+        reflection = self.class._reflect_on_association(name)
+
+        # if reflection is nil, try to get the parent class' reflection
+        klass = self.class.superclass
+        while reflection.nil? && klass.respond_to?(:_reflect_on_association)
+          reflection = klass._reflect_on_association(name)
+          klass = klass.superclass
+        end
+
+        unless reflection
+          raise AssociationNotFoundError.new(self, name)
+        end
+
+        association = reflection.association_class.new(self, reflection)
+        association_instance_set(name, association)
+      end
+
+      association
+    end
+  end
+end
+
 # Copyright Orange 2024
 Redmine::Plugin.register :attribute_category do
   name 'Attribute Category plugin'
@@ -9,16 +41,16 @@ Redmine::Plugin.register :attribute_category do
 
   # WARNING in redmine 4.x Rails.configuration.to_prepare
   Rails.application.config.after_initialize do
-    Project.send(:include, ProjectModelPatch)
-    ProjectsController.send(:include, ProjectsControllerPatch)
-    Tracker.send(:include, TrackerModelPatch)
-    CustomField.send(:include, CustomFieldModelPatch)
-    CustomFieldsHelper.send(:include, CustomFieldsHelperPatch)
+    Project.include ProjectModelPatch
+    ProjectsController.include ProjectsControllerPatch
+    Tracker.include TrackerModelPatch
+    CustomField.include CustomFieldModelPatch
+    CustomFieldsHelper.include CustomFieldsHelperPatch
     CustomField.safe_attributes :group_category_layout
-    IssuesHelper.send(:include, IssuesHelperPatch)
+    IssuesHelper.include IssuesHelperPatch
   end
 
-  menu :admin_menu, :attribute_categories, { controller: 'attribute_categories', action: 'index' }, caption: 'Attribute Categories'
+  menu :admin_menu, :attribute_categories, { controller: :attribute_categories, action: :index }, caption: 'Attribute Categories'
 
   permission :group_issues_custom_fields, {:group_issues_custom_fields => [:group_issues_custom_fields, :index], :projects => [:save_group_issues_custom_fields]}, :require => :member
   menu :project_menu, :group_issues_custom_fields, { controller: :group_issues_custom_fields, action: :index }, caption: :grouped_cf, after: :setting, param: :project_id  
